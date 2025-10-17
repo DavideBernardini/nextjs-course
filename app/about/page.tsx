98dc007908d3a30608636d2153f5a9be1aefc324
DavideBernardini <davidebernardini09@gmail.com>
2025-10-17 17:40:51 +0200

    metadata

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About us",
+  description: "i can p[ut keywords here",
+  keywords: "about, corporate, openai",
+  twitter: {
+    card: "summary_large_image",
+    title: "About us",
+  },
+};
+
 const About = () => {
   return <div>About page</div>;
 };
